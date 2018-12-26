acdc451ddb1ad46f4de53faabcc0bdc2736cc421
Vermee81 <hrksb5029@gmail.com>
2018-12-27 01:27:15 +0900

    [add] Add a button for rating
    レーティングのボタンを追加。
    一時的に赤いボタンにしている。

---
+++ FoodTracker/RatingControl.swift
@@ -0,0 +1,40 @@
+//
+//  RatingControl.swift
+//  FoodTracker
+//
+//  Created by Hiroo Kusaba on 2018/12/27.
+//  Copyright © 2018年 Hiroo Kusaba. All rights reserved.
+//
+
+import UIKit
+
+class RatingControl: UIStackView {
+
+    // MARK: Initialization
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupButtons()
+    }
+    
+    required init(coder: NSCoder) {
+        super.init(coder: coder)
+        setupButtons()
+    }
+    
+    // MARK: Private Methods
+    
+    private func setupButtons(){
+        // Create the button
+        let button = UIButton()
+        button.backgroundColor = UIColor.red
+        
+        // Add constraints
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.heightAnchor.constraint(equalToConstant: 44.0).isActive = true
+        button.widthAnchor.constraint(equalToConstant: 44.0).isActive = true
+        
+        // Add the button to the stack
+        addArrangedSubview(button)
+    }
+    
+}
